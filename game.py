fccae2657b0c0c03e0af2ec34f151504b2abc2a4
cnm370 <62782633+cnm370@users.noreply.github.com>
2022-07-29 00:18:00 -0400

    Testing (#2)
    * Game test (failing for now)
    
    * Winner function returns some things and can be tested
    
    * Fix bug in our functions, test are passing
    
    * Use the function we tested / fix the bus for real in our game

--- game.py
+++ game.py
@@ -9,45 +9,76 @@ from random import choice
 # so we aren't able to forget to update all the places
 valid_choices = ["rock", "paper", "scissors"]
 
+def winner(user_choice, computer_choice):
+    if user_choice == "rock" and computer_choice == "rock":
+        #print("It's a tie!")
+        return "It's a tie!"
+    elif user_choice == "rock" and computer_choice == "paper":
+        #print("The computer wins")
+        return "The computer wins"
+    elif user_choice == "rock" and computer_choice == "scissors":
+        #print("The user wins")
+        return "The user wins"
 
-#
-# USER SELECTION
-#
+    elif user_choice == "paper" and computer_choice == "rock":
+        #print("The computer wins")
+        #return "The Computer wins" # BUG!! OOPS :-)
+        return "The user wins"
+    elif user_choice == "paper" and computer_choice == "paper":
+        #print("It's a tie!")
+        return "It's a tie!"
+    elif user_choice == "paper" and computer_choice == "scissors":
+        #print("The user wins")
+        #return "The user wins" # BUG!! OOPS :-)
+        return "The computer wins"
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in valid_choices:
-    print("OOPS, TRY AGAIN")
-    exit()
+    elif user_choice == "scissors" and computer_choice == "rock":
+        #print("The computer wins")
+        return "The computer wins"
+    elif user_choice == "scissors" and computer_choice == "paper":
+        #print("The user wins")
+        return "The user wins"
+    elif user_choice == "scissors" and computer_choice == "scissors":
+        #print("It's a tie!")
+        return "It's a tie!"
 
-#
-# COMPUTER SELECTION
-#
 
-c = choice(valid_choices)
-print("COMPUTER CHOICE:", c)
+if __name__ == "__main__":
 
-#
-# DETERMINATION OF WINNER
-#
+        
+    # ONLY  RUN THE FOLLOWING CODE IF WE RUN THIS SCRIPT FROM THE COMMAND LINE
+    # BUT NOT IF WE ARE IMPORTING SOME CODE FROM THIS FILE TO ANOTHER FILE
+    # PREVENTS EXECUTION OF THE CODE BELOW WHEN ALL WE WANT TO DO IS IMPORT / TEST A FUNCTION IN ISOLATION
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+    #
+    # USER SELECTION
+    #
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in valid_choices:
+        print("OOPS, TRY AGAIN")
+        exit()
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+    #
+    # COMPUTER SELECTION
+    #
+
+    c = choice(valid_choices)
+    print("COMPUTER CHOICE:", c)
+
+    #
+    # DETERMINATION OF WINNER
+    #
+
+
+
+    # use the function from abvove???
+
+print(winner(u, c))
+
+#message = winner(u, c)
+#print(message)
+
+
+

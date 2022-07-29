3b41d61c2db53394d410d5f1230b555546ad54cf
cnm370 <cnm370@stern.nyu.edu>
2022-07-29 00:15:10 -0400

    Use the function we tested / fix the bus for real in our game

--- game.py
+++ game.py
@@ -71,23 +71,14 @@ if __name__ == "__main__":
     # DETERMINATION OF WINNER
     #
 
-    if u == "rock" and c == "rock":
-        print("It's a tie!")
-    elif u == "rock" and c == "paper":
-        print("The computer wins")
-    elif u == "rock" and c == "scissors":
-        print("The user wins")
 
-    elif u == "paper" and c == "rock":
-        print("The computer wins")
-    elif u == "paper" and c == "paper":
-        print("It's a tie!")
-    elif u == "paper" and c == "scissors":
-        print("The user wins")
 
-    elif u == "scissors" and c == "rock":
-        print("The computer wins")
-    elif u == "scissors" and c == "paper":
-        print("The user wins")
-    elif u == "scissors" and c == "scissors":
-        print("It's a tie!")
+    # use the function from abvove???
+
+print(winner(u, c))
+
+#message = winner(u, c)
+#print(message)
+
+
+
